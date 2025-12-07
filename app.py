f12889fb4f58f3733b209a1f84da3dd96248ae33
unknown <24f2001013@ds.study.iitm.ac.in>
2025-12-07 16:43:39 +0530

    data

--- app.py
+++ app.py
@@ -272,7 +272,8 @@ def get_all_users():
 
 # ==================== RUN APP ====================
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, host='0.0.0.0', port=5000)
